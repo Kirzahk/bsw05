a42e7e4fedc9cc47f71040133418d65b6a7f5de4
Kirzahk <61593550+Kirzahk@users.noreply.github.com>
2020-03-01 20:15:45 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Ejemplo006___format_string
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string applesText = "Apples";
+            int applesCount = 1234;
+            string bananasText = "Bananas";
+            int bananasCount = 56789;
+            Console.WriteLine(
+                format: "{0,-8} {1,6:N0}",
+                arg0: "Name",
+                arg1: "Count");
+
+            Console.WriteLine(
+                format: "{0,-8} {1,6:N0}",
+                arg0: applesText,
+                arg1: applesCount);
+            
+            Console.WriteLine(
+                format: "{0,-8} {1,6:N0}",
+                arg0: bananasText,
+                arg1: bananasCount);
+            }
+    }
+}
